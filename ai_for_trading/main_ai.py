1b9df7529b9c95ddb10a6b744b0d61b0d3aa3c5c
TyKo0707 <super.family.timut@gmail.com>
2021-11-19 22:56:09 +0200

    updating ai for trading 4

--- ai_for_trading/main_ai.py
+++ ai_for_trading/main_ai.py
@@ -59,7 +59,7 @@ class AIPMDevelopment:
 
     def __init__(self):
         # Read your data in and split the dependent and independent
-        data = pd.read_csv('IBM.csv')
+        data = pd.read_csv('test.csv')
         X = data['Delta Close']
         y = data.drop(['Delta Close'], axis=1)
 
